8a7e73204e02fb86d0527b36e9812570d8ab5499
R3moulade <91252465+R3moulade@users.noreply.github.com>
2023-05-04 14:54:39 +0200

    4 maj

--- Hitbox.gd
+++ Hitbox.gd
@@ -1,16 +1,5 @@
 extends Area2D
 class_name Hitbox
 
-
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 func _on_Hitbox_area_entered(area: Area2D) -> void:
 	pass
--- Main.gd
+++ Main.gd
@@ -4,13 +4,17 @@ class_name Main
 export (PackedScene) var hitbox_scene
 var hitbox_spawn_location
 var score = 0 setget set_score
-onready var timerLabel = $Slider/TimerLabel
-onready var hudContainer = $HUD/HUDContainer
-onready var countdownTimer = $CountdownTimer
-onready var collisionTimer = $CollisionTimer
-onready var endTimer = $End/EndTimer
-onready var hudTimer = $HUD/HUDContainer/Timer
-onready var hudScore = $HUD/HUDContainer/Score
+var highscore = 0 setget set_highscore
+onready var timerLabel := $Slider/TimerLabel
+onready var hudContainer := $HUD/HUDContainer
+onready var countdownTimer := $CountdownTimer
+onready var collisionTimer := $Slider/CollisionTimer
+onready var endTimer := $End/EndTimer
+onready var hudTimer := $HUD/HUDContainer/Timer
+onready var hudScore := $HUD/HUDContainer/Score
+onready var animations := $AnimationPlayer
+var time_now = 0
+var time_start = 0
 
 func _ready():
 	randomize()
@@ -18,32 +22,44 @@ func _ready():
 	timerLabel.hide()
 	hudContainer.hide()
 	self.score = -10
+	$Slider/CollisionProgress.value = 0.0
 	
 	
 func _process(delta: float) -> void:
-	timerLabel.text = str(int(collisionTimer.time_left + 1))
+	time_now = OS.get_unix_time()
+	var elapsed = time_now - time_start
+	var seconds = elapsed % 60
+	var str_elapsed = "%2.0d" % seconds
+	print("elapsed : ", str_elapsed)
+	
 	hudTimer.text = str(int(countdownTimer.time_left))
+	$Slider/CollisionProgress.value = str_elapsed
 
 #func _set(property: String, value) -> bool:
 	
 func _on_CollisionTimer_timeout():
-	var hitbox = hitbox_scene.instance()
-	
-	#adds a hitbox at a random spot on the path
-	hitbox_spawn_location.unit_offset = randf()
-	hitbox_spawn_location.add_child(hitbox)
-	hitbox.global_position = hitbox_spawn_location.global_position
-	
-	#updates the score. self accesses the setter method
-	self.score += 10
-	
-	#Removes the hitbox that was collisioned
-	if hitbox_spawn_location.get_child_count() == 2:
-		hitbox.queue_free()
+	#if the end scene is not playing...
+	#prevents a bug on the animationplayer and continuation of play in the background.
+	if animations.get_current_animation() != "EndScene":
+		var hitbox = hitbox_scene.instance()
+		
+		#adds a hitbox at a random spot on the path
+		hitbox_spawn_location.unit_offset = randf()
+		hitbox_spawn_location.add_child(hitbox)
+		hitbox.global_position = hitbox_spawn_location.global_position
+		
+		#updates the score. self accesses the setter method
+		self.score += 10
+		#character jumps
+		animations.play("Nansen")
+		
+		#Removes the hitbox that was collisioned
+		if hitbox_spawn_location.get_child_count() == 2:
+			hitbox.queue_free()
 
 #Fires when the score value is changed
-func set_score(value):
-	score = value
+func set_score(s_value):
+	score = s_value
 	hudScore.set_text(str(score))
 	#Game start
 	if score == 0:
@@ -52,19 +68,35 @@ func set_score(value):
 		countdownTimer.one_shot = true
 		hudContainer.show()
 
+func set_highscore(hs_value):
+	highscore = hs_value
+
+#when the arrow enters the hitbox, start the timer
 func _on_Hitbox_area_entered(area: Area2D) -> void:
 	collisionTimer.start()
 	timerLabel.show()
+	time_start = OS.get_unix_time()
 
+#when the arrow leaves the hitbox, restart the timer
 func _on_Hitbox_area_exited(area: Area2D) -> void:
 	collisionTimer.stop()
 	timerLabel.hide()
+	time_start = 0
 
+#when the game is over
 func _on_CountdownTimer_timeout() -> void:
 	endTimer.start()
 	endTimer.one_shot = true
-	
-
+	$End/EndContainer/FinalScore.set_text(str(self.score))
+	animations.play("EndScene")
+	#sets the highscore
+	if self.score > self.highscore:
+		highscore = score
+		$End/EndContainer/Highscore.set_text("Ny highscore! \n" + str(self.highscore))
+	else:
+		$End/EndContainer/Highscore.set_text("Highscore \n" + str(self.highscore))
+		
+#when the full loop of the game is over
 func _on_EndTimer_timeout() -> void:
 	self.score = -10
 	endTimer.one_shot = false
