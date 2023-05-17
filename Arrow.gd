a0135836bf2f5e9be2679e45c672f0ade993e143
R3moulade <91252465+R3moulade@users.noreply.github.com>
2023-05-17 16:41:40 +0200

    a bunch

--- Arrow.gd
+++ Arrow.gd
@@ -6,6 +6,7 @@ var slider_size = Vector2.ZERO
 func _ready():
 	slider_size = get_parent().rect_size
 	print(slider_size)
+	
 
 func _process(delta):
 	var direction : = Vector2.ZERO
@@ -18,3 +19,10 @@ func _process(delta):
 	
 	#Limits the arrow's position to the parent (the slider)
 	position.x = clamp(position.x, 86, slider_size.x - 86)
+	
+	#if Input.is_action_pressed("move_right"):
+		#unit_offset += 0.01
+	#if Input.is_action_pressed("move_left"):
+		#unit_offset -= 0.01
+
+		#$Arrow.position += unit_offset * direction * speed * delta
--- Main.gd
+++ Main.gd
@@ -5,7 +5,6 @@ export (PackedScene) var hitbox_scene
 var hitbox_spawn_location
 var score = 0 setget set_score
 var highscore = 0 setget set_highscore
-onready var timerLabel := $Slider/TimerLabel
 onready var hudContainer := $HUD/HUDGame
 onready var hudStandby := $HUD/HUDStandby
 onready var countdownTimer := $CountdownTimer
@@ -17,19 +16,26 @@ onready var animations := $AnimationPlayer
 onready var animations2 := $AnimationPlayer2
 onready var animations3 := $AnimationPlayer3
 onready var animations4 := $AnimationPlayer4
+onready var arrow := $Slider/Slider/Arrow
+onready var hitbox := $Slider/Slider/HitboxPath/HitboxSpawnLocation/Hitbox
+onready var fiveSecondsLeft := $Slider/CountdownTimer5sec
+onready var between := $Slider/Slider/HitboxPath/HitboxSpawnLocation/Hitbox/Between
+onready var middle := $Slider/Slider/HitboxPath/HitboxSpawnLocation/Hitbox/Middle
 
 
 func _ready():
 	randomize()
 	hitbox_spawn_location = $Slider/Slider/HitboxPath/HitboxSpawnLocation
-	timerLabel.hide()
 	hudContainer.hide()
 	self.score = -10
 	
 func _process(delta: float) -> void:
-	hudTimer.text = "0:" + str(int(countdownTimer.time_left))
-#func _set(property: String, value) -> bool:
-	
+	if countdownTimer.time_left > 0:
+		fiveSecondsLeft.text = str(int(countdownTimer.time_left) + 1)
+	if hitbox.overlaps_area(arrow):
+		if !animations2.is_playing() && animations.get_current_animation() != "EndScene":
+			animations2.play("CollisionProgress")
+
 func _on_CollisionTimer_timeout():
 	#if the end scene is not playing...
 	#prevents a bug on the animationplayer and continuation of play in the background.
@@ -42,7 +48,15 @@ func _on_CollisionTimer_timeout():
 		hitbox.global_position = hitbox_spawn_location.global_position
 		
 		#updates the score. self accesses the setter method
-		self.score += 10
+		if score != -10:
+			if arrow.overlaps_area(between) && !arrow.overlaps_area(middle):
+				self.score += 15
+			elif arrow.overlaps_area(middle):
+				self.score += 30
+			else:
+				self.score += 5
+		else:
+			self.score = 0
 		#character jumps
 		animations2.play("RESET")
 		animations.play("Nansen")
@@ -62,7 +76,7 @@ func set_score(s_value):
 		countdownTimer.one_shot = true
 		hudContainer.show()
 		hudStandby.hide()
-		animations3.play("GameStart")
+		animations4.play("RESET")
 		animations4.play("CountdownProgress")
 		
 
@@ -70,15 +84,14 @@ func set_highscore(hs_value):
 	highscore = hs_value
 
 #when the arrow enters the hitbox, start the timer
-func _on_Hitbox_area_entered(area: Area2D) -> void:
-	collisionTimer.start()
-	timerLabel.show()
-	animations2.play("CollisionProgress")
+#func _on_Hitbox_area_entered(area: Area2D) -> void:
+#	collisionTimer.start()
+#	timerLabel.show()
+#	animations2.play("CollisionProgress")
 
 #when the arrow leaves the hitbox, restart the timer
 func _on_Hitbox_area_exited(area: Area2D) -> void:
 	collisionTimer.stop()
-	timerLabel.hide()
 	animations2.play("RESET")
 
 #when the game is over
@@ -87,6 +100,7 @@ func _on_CountdownTimer_timeout() -> void:
 	endTimer.one_shot = true
 	$End/EndContainer/FinalScore.set_text(str(self.score))
 	animations.play("EndScene")
+	fiveSecondsLeft.text = "Tiden er gået!"
 	#sets the highscore
 	if self.score > self.highscore:
 		highscore = score
@@ -101,7 +115,4 @@ func _on_EndTimer_timeout() -> void:
 	endTimer.one_shot = false
 	hudContainer.hide()
 	hudStandby.show()
-	animations3.play("RESET")
 	animations4.play("RESET")
-
-
